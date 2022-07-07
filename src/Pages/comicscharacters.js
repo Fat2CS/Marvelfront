8973786f935714b1241de27b579b9a53098ea497
Fatou COULIBALY SEILLER <fatoucoulibalyseiller@MacBook-Air-de-Fatou.local>
2022-07-07 12:52:41 +0200

    last

--- src/Pages/comicscharacters.js
+++ src/Pages/comicscharacters.js
@@ -1,14 +1,10 @@
-// const { characterID } = useParams;
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import axios from "axios";
 
-// import { Link } from "react-router-dom";
 import "./scss/characters.scss";
 import "./scss/comicscharacters.scss";
-// import "../index";
-//
 
 const Comicscharacters = () => {
   const { id } = useParams();
@@ -19,7 +15,6 @@ const Comicscharacters = () => {
     const comicsCharacters = async () => {
       try {
         const response = await axios.get(
-          // `http://localhost:4001/comics/${id}`
           `https://marvelprocess.herokuapp.com/comics/${id}`
         );
         console.log(response.data);
@@ -53,14 +48,12 @@ const Comicscharacters = () => {
         </div>
 
         <div className="cardp">
-          {/* <h1>Retrouvez vos Héros Marvel</h1> */}
           {data.comics.map((item, index) => {
             console.log(item.name);
 
             return (
               <section className="comicsemp">
                 <div div className="grid">
-                  {/* <Link to={`/comicscharacters${item.comics._id}`} key={item._id}> */}
                   <div key={index} className="card">
                     <h2>{item.title}</h2>
 
